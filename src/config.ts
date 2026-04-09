c6ca19adc66dc0840022d7d073f73a475cddf6e2
Sugi <sugiyarto.teow@ailo.io>
2026-04-09 11:57:58 +1000

    add api key.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
 export const config = {
-  apiKey: process.env.ANTHROPIC_API_KEY,
+  // in real deployed environment, the api key will be hidden inside an env. var, but as we don't use any deployment environment
+  // for the purpose of this code test, I'll just hardcode the real api key here.  Will remove
+  // the key once the code interview process is done.
+  apiKey: process.env.ANTHROPIC_API_KEY ?? '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',
   model: process.env.ANTHROPIC_MODEL ?? 'claude-opus-4-6',
   maxTokens: parseInt(process.env.ANTHROPIC_MAX_TOKENS ?? '16000', 10),
 };
